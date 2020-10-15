38a26c079a859c1bd2d8c1550e28b1477223ce15
Xillians <chkxillians@gmail.com>
2020-10-15 17:32:51 +0200

    changed profile picture and added devi's social for credit.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img alt="Vue logo" src="./assets/logo.png" class="logo" /><br />
+  <div class="note">
+    <a>Profile picture is drawn by DevicatOutlet. <br /></a>
+    <a>You can find her on </a>
+    <a href="https://www.twitch.tv/devicat" target="_blank" rel="noopener"
+      >twitch</a
+    >
+    <a> or </a>
+    <a href="https://twitter.com/DeviCatOutlet" target="_blank" rel="noopener"
+      >twitter
+    </a>
+  </div>
   <HelloWorld msg="Xillians portfolio" />
 </template>
 
@@ -23,5 +34,20 @@ export default class App extends Vue {}
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  align-content: center;
+  height: 250 px;
+  width: 250px;
+}
+
+.note {
+  font-family: Avenit, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: darkslategrey;
+  font-size: 12px;
 }
 </style>
