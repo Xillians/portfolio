e4d58538349d09ac0c06b4c2846010cbd1cbba91
Xillians <chkxillians@gmail.com>
2020-10-19 16:59:32 +0200

    changed imgur gallery to be an option list instead of hard coded html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,60 +2,30 @@
   <div class="helloWorld">
     <h1>{{ msg }}</h1>
     <p>
-      Welcome to my portfolio! You can find me on the following locations<br>
-      <a 
-        href="https://github.com/Xillians" 
-        target="_blank" 
-        rel="noopener"
+      Welcome to my portfolio! You can find me on the following locations<br />
+      <a href="https://github.com/Xillians" target="_blank" rel="noopener"
         >github</a
-      ><br>
-      <a 
-        href="https://twitter.com/xillians" 
-        target="_blank" 
-        rel="noopener"
+      ><br />
+      <a href="https://twitter.com/xillians" target="_blank" rel="noopener"
         >twitter</a
-      ><br>
-      <a 
-        href="https://www.twitch.tv/xillians" 
-        target="_blank" 
-        rel="noopener"
+      ><br />
+      <a href="https://www.twitch.tv/xillians" target="_blank" rel="noopener"
         >twitch</a
-      ><br>
+      ><br />
     </p>
-    <h3>imgur gallery</h3>
+  </div>
+  <div class="Gallery">
     <ul>
-      <img class="thumbnail" src="https://i.imgur.com/SGfSfme.jpg" alt="dagger"><br>
-      <a
-        href="https://imgur.com/a/LSnhoHN"
-        target="_blank"
-        rel="noopener"
-        >daedric dagger</a
-      >
-    </ul>
-    <ul>
-      <img class="thumbnail" src="https://i.imgur.com/XX5xWgb.jpg" alt="dagger"><br>
-      <a
-        href="https://imgur.com/a/PCLLwz6"
-        target="_blank"
-        rel="noopener"
-        >chain chomp</a
-      >
-    </ul>
-    <ul>
-      <img class="thumbnail" src="https://i.imgur.com/ylVck7d.jpg" alt="dagger"><br>
-      <a
-        href="https://imgur.com/a/EyxGB4d"
-        target="_blank"
-        rel="noopener"
-        >samus aran</a
-      >
+      <li v-for="item in gallery" :key="item.name">
+        <img class="thumbnail" :src="item.img" :alt="item.name" /><br />
+        <a :href="item.href" target="_blank" rel="noopener">{{item.name}}</a>
+      </li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-
 @Options({
   props: {
     msg: String
@@ -63,6 +33,29 @@ import { Options, Vue } from "vue-class-component";
 })
 export default class HelloWorld extends Vue {
   msg!: string;
+}
+export class Gallery extends Vue {
+  data() {
+    return {
+      gallery: [
+        {
+          img: "https://i.imgur.com/SGfSfme.jpg",
+          href: "https://imgur.com/a/LSnhoHN",
+          name: "daedric dagger"
+        },
+        {
+          img: "https://i.imgur.com/XX5xWgb.jpg",
+          href: "https://imgur.com/a/PCLLwz6",
+          name: "chain chomp"
+        },
+        {
+          img: "https://i.imgur.com/ylVck7d.jpg",
+          href: "https://imgur.com/a/EyxGB4d",
+          name: "samus aran"
+        }
+      ]
+    };
+  }
 }
 </script>
 
@@ -83,6 +76,7 @@ a {
   color: #42b983;
 }
 .thumbnail {
+  border-radius: 50px;
   width: 500px;
   height: 300px;
 }
