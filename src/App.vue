da2a8e6a1ffdf63801ca9ce13284b8d3ce814766
Xillians <chkxillians@gmail.com>
2022-08-26 09:41:58 +0200

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -17,11 +17,11 @@ export default class App extends Vue {}
 
 <style lang="scss">
 #app {
+  @import "../public/bootstrap.css";
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
