6591ddda024af7b05403f71bf5120b8c92cd8855
Xillians <sandra.toftemo@bjerk.io>
2025-02-19 13:18:28 +0100

    use navbar and stylesheet

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
+<script setup>
+import Navbar from "./components/NavBar.vue";
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navbar />
   <router-view />
 </template>
 
+<style src="./stylesheet.css"></style>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--font-family);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--text-color);
 }
 
 nav {
   padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
