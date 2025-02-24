b994635634980a346238b240859ac75992eb83e9
Xillians <sandra.toftemo@bjerk.io>
2025-02-24 16:40:32 +0100

    start filling about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About me</h1>
+    <img src="../assets/pfp.jpeg" alt="Profile Picture" />
   </div>
 </template>
+
+<style scoped>
+img {
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+  display: block;
+}
+</style>
