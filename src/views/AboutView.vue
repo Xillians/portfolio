fd6132074c4a5116632547c0bf13d6f345388fb4
Xillians <sandra.toftemo@bjerk.io>
2025-02-24 17:20:41 +0100

    update about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="about">
-    <h1>About me</h1>
+    <h2>About me</h2>
     <img src="../assets/pfp.jpeg" alt="Profile Picture" />
+    <p>
+      I am a software developer from Oslo, Norway. I love lots of nerdy things
+      such as software development, stationeries, tabletop games, and more.
+    </p>
+    <h2>Get in touch!</h2>
+    <p>
+      You can reach me out on the following social medias, or send me an
+      <a href="mailto:sandra.toftemo@gmail.com">email</a>
+    </p>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/sandra-viktoria-doseth-toftemo-b9b946227/"
+          target="_blank"
+          >LinkedIn</a
+        >
+      </li>
+      <li>
+        <a href="https://bsky.app/profile/xillians.bsky.social" target="_blank"
+          >Bluesky</a
+        >
+      </li>
+      <li>
+        <a href="https://www.instagram.com/thexillyone/" target="_blank"
+          >Instagram</a
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,5 +40,9 @@ img {
   height: 200px;
   margin: 0 auto;
   display: block;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
 }
 </style>
